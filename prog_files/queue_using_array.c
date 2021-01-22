9ce9a4f11bcdac001134f35559f8dfb93856d7d1
Abhishek Gond <abhishek.kvisha@gmail.com>
2021-01-22 16:03:18 +0530

    added

--- prog_files/queue_using_array.c
+++ prog_files/queue_using_array.c
@@ -1,40 +1,54 @@
-#include<stdio.h>
-#define SIZE 10
+//Implimentation of Queue using array 
 
-void enQueue(int);
+#include<stdio.h>
+//#define SIZE 8
+
+void enQueue(int, int);
 void deQueue();
 void display();
+int queue[0];
 
-int queue[SIZE], front = -1, rear = -1;
+int front = -1, rear = -1;
 
 int main()
 {
-   int value, choice;
+   int value, choice,SIZE;
+
+   printf("\nEnter size of STACK:- ");
+   scanf("%d",&SIZE);
+   int queue_var[SIZE];
+   *queue = *queue_var;
    
-   while(1){
+   while(1) {
       printf("\n\n***** WELCOME TO MENU *****\n");
       printf("1. Insertion\n2. Deletion\n3. Display\n4. Exit");
       printf("\nEnter your choice: ");
       scanf("%d",&choice);
-      switch(choice){
+
+      switch (choice) {
 	 case 1: printf("Enter the value to be insert: ");
-		 scanf("%d",&value);
-		 enQueue(value);
-		 break;
+		       scanf("%d",&value);
+		       enQueue(value,SIZE);
+		       break;
+
 	 case 2: deQueue();
-		 break;
+		       break;
+
 	 case 3: display();
 		 break;
+
 	 case 4:
+    printf("\n!!!Thank You , Have a Nice Day!!!");
          return 0;
+         
 	 default: printf("\nWrong selection!!! Try again!!!");
       }
    }
 }
-void enQueue(int value){
-   if(rear == SIZE-1)
+void enQueue(int value, int SIZE){
+   if (rear == SIZE-1)
       printf("\nQueue is Full!!! Insertion is not possible!!!");
-   else{
+   else {
       if(front == -1)
 	 front = 0;
       rear++;
@@ -42,20 +56,24 @@ void enQueue(int value){
       printf("\nInsertion success!!!");
    }
 }
-void deQueue(){
-   if(front == rear)
+void deQueue() {
+   if (front == rear)
+   {
+
+      printf("\nDeleted : %d", queue[front]);
       printf("\nQueue is Empty!!! Deletion is not possible!!!");
+      front = rear = -1;
+   }
    else{
       printf("\nDeleted : %d", queue[front]);
       front++;
-      if(front == rear)
-	 front = rear = -1;
+     
    }
 }
-void display(){
-   if(rear == -1)
+void display() {
+   if (rear == -1)
       printf("\nQueue is Empty!!!");
-   else{
+   else {
       int i;
       printf("\nQueue elements are:\n");
       for(i=front; i<=rear; i++)
--- prog_files/stack_using_array.c
+++ prog_files/stack_using_array.c
@@ -1,71 +1,69 @@
-//Stack using array all operations 
+//Implimentation of Stack using array all operations 
 #include<stdio.h>
-#include<stdlib.h>
 
-void push(int, int);
+//#define SIZE 10
+
+void push(int,int);
 void pop();
 void display();
 int top = -1;
 int stack[0];
 
-int main() {
-   int value, choice, SIZE;
+int main()
+{
+   int value, choice,SIZE;
    
    printf("\nEnter size of STACK:- ");
-   scanf("%d", &SIZE);
+   scanf("%d",&SIZE);
    int stack_var[SIZE];
    *stack = *stack_var;
    
-   while(1) {
+   while(1){
       printf("\n\n***** WELCOME TO MENU *****\n");
       printf("\nHow many elements you want to Enter?");
       printf("\n1. Push\n2. Pop\n3. Display\n4. Exit");
       printf("\nEnter your choice: ");
-      scanf("%d", &choice);
-      switch (choice) {
-
-	 case 1: printf("Enter the value to be insert: ");
-		       scanf("%d", &value);
-		       push(SIZE, value);
-		       break;
-
-	 case 2: pop();
-		       break;
-
-	 case 3: display();
-		       break;
-
-	 case 4: exit(0);
-
-	 default: printf("\nWrong selection!!! Try again!!!");
+      scanf("%d",&choice);
+      switch(choice){
+    case 1: printf("Enter the value to be insert: ");
+       scanf("%d",&value);
+       push(SIZE,value);
+       break;
+    case 2: pop();
+       break;
+    case 3: display();
+       break;
+    case 4: 
+    printf("\n!!!Thank You , Have a Nice Day!!!");
+    return 0;
+    default: printf("\nWrong selection!!! Try again!!!");
       }
    }
 }
-void push(int SIZE, int value){
-   if (top == SIZE-1)
+void push(int SIZE ,int value){
+   if(top == SIZE-1)
       printf("\nStack is Full!!! Insertion is not possible!!!");
-
-   else {
+   else{
       top++;
       stack[top] = value;
       printf("\nInsertion success!!!");
    }
 }
-void pop() {
-   if (top == -1)
+void pop(){
+   if(top == -1)
       printf("\nStack is Empty!!! Deletion is not possible!!!");
-   else {
+   else{
       printf("\nDeleted : %d", stack[top]);
       top--;
    }
 }
 void display(){
-   if (top == -1)
+   if(top == -1)
       printf("\nStack is Empty!!!");
-   else {
+   else{
       int i;
       printf("\nStack elements are:\n");
-      for (i = top; i >= 0; i--)
-	 printf("%d\n", stack[i]);
+      for(i=top; i>=0; i--)
+    printf("%d\n",stack[i]);
    }
 }
